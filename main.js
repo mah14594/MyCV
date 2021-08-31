8f4d473b900cc3257299209ecd50fbb6cdd20112
alsalem.mahdi94@gmail.com <you@example.com>
2021-08-31 03:09:53 +0300

    edit the transition of the skills

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ for (i=0;i<details.length;i++){
 function showskills(elem) {
     var id=elem.id;
    //for loop to give a sequence of transition for the skills details 
-    var det=document.querySelectorAll("#"+id+"skills"+" "+"p:nth-child(n)") //select all p elements in this father
+    var det=document.querySelectorAll("#"+id+"skills"+" "+"p:nth-child(n)") //select all p elements in this
     for (var i=0;i<det.length;i++){
         console.log(det[i]);
         det[i].style.transition=((i+1)/20)+"s ease"
