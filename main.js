904154e6c437709284be9036c0d1785b2e907183
alsalem.mahdi94@gmail.com <you@example.com>
2021-09-21 03:26:39 +0300

    add projects section

--- main.js
+++ main.js
@@ -21,23 +21,28 @@ var details=skills.querySelectorAll("i");
 for (i=0;i<details.length;i++){
     details[i].classList.add("fa-plus");
 }
+
+
 function showskills(elem) {
     var id=elem.id;
-   //for loop to give a sequence of transition for the skills details 
-    var det=document.querySelectorAll("#"+id+"skills"+" "+"p:nth-child(n)") //select all p elements in this
-    for (var i=0;i<det.length;i++){
-        console.log(det[i]);
-        det[i].style.transition=((i+1)/20)+"s ease"
-    }
-    console.log(det);
-    if (document.getElementById(id).className == "fa fa-plus") {
-        document.getElementById(id).className = "fa fa-times";
-        document.getElementById(id+"skills").style.height = "100%";
+    var skill_details=document.getElementById(id);
+    if (skill_details.className == "fa fa-plus") {
+        // var pixheight=document.getElementById("skills").clientHeight;
+        // document.getElementById("skills").setAttribute("height",pixheight+200)
+        skill_details.className = "fa fa-times";
+        document.getElementById(id+"skills").setAttribute("style","height:100%") //.style.height="100% "may not work in some browsers
     }
     else {
-        document.getElementById(id).className = "fa fa-plus";
-        document.getElementById(id+"skills").style.height = "0";
+        skill_details.className = "fa fa-plus";
+        document.getElementById(id+"skills").setAttribute("style","height:0");
+        }
 
+    //for loop to give a sequence of transition for the skills details 
+  /* 
+   var det=document.querySelectorAll("#"+id+"skills"+" "+"p:nth-child(n)") //select all p elements in this
+    for (var i=0;i<det.length;i++){
+        det[i].style.transition=((i+1)/20)+"s ease";
     }
-
+    */
+   
 }
